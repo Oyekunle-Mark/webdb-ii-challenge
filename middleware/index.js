1b94280de89fd53aec395c02f47fe52f7c08bdfd
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-16 16:05:13 +0100

    Added validation middleware to the route

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,46 @@
+const Account = require('../model');
+
 const badUrl = (req, res) =>
   res.status(404).json({
     status: 404,
     message: 'Take a good look at the URL!',
   });
 
+const verifyId = (req, res, next) => {
+  const { id } = req.params;
+
+  if (!/^\d+$/.test(id))
+    return res.status(400).json({
+      status: 400,
+      message: 'Request parameter must be an integer.',
+    });
+
+  Account.get(id).then(car => {
+    if (!car.length)
+      return res.status(404).json({
+        status: 404,
+        message: 'No account matches that Id.',
+      });
+
+    next();
+  });
+};
+
+const verifyCar = (req, res, next) => {
+  const { VIN, make, model, mileage, status, transmission } = req.body;
+
+  if (!VIN || !make || !model || !mileage || !status || !transmission)
+    return res.status(400).json({
+      status: 400,
+      message:
+        'Request body must contain  VIN, make, model, mileage, status and transmission fields.',
+    });
+
+  next();
+};
+
 module.exports = {
   badUrl,
+  verifyId,
+  verifyCar,
 };
--- router/index.js
+++ router/index.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
 
 const handlers = require('./handlers');
+const middleware = require('../middleware');
 
 router.get('/', handlers.getCars);
-router.get('/:id', handlers.getCarById);
-router.post('/', handlers.createCar);
-router.put('/:id', handlers.updateCar);
-router.delete('/:id', handlers.removeCar);
+router.get('/:id', middleware.verifyId, handlers.getCarById);
+router.post('/', middleware.verifyCar, handlers.createCar);
+router.put(
+  '/:id',
+  middleware.verifyId,
+  middleware.verifyCar,
+  handlers.updateCar,
+);
+router.delete('/:id', middleware.verifyId, handlers.removeCar);
 
 module.exports = router;
